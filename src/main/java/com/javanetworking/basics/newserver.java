e75e1dda8d78de03e2af2db253dee2c03d2f849d
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-11-14 22:46:06 -0300

    correções

--- src/main/java/com/javanetworking/basics/newserver.java
+++ src/main/java/com/javanetworking/basics/newserver.java
@@ -3,21 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.javanetworking.basics;
-import io.fusionauth.http.server.HTTPHandler;
 import io.fusionauth.http.server.HTTPListenerConfiguration;
 import io.fusionauth.http.server.HTTPServer;
-import io.fusionauth.http.server.HTTPServerConfiguration;
-import java.net.InetAddress;
 /**
  *
  * @author Erick
  */
 public class newserver {
     public static void main(String[] args) throws Exception{
-   HTTPServer serv = new HTTPServer().withListener(new HTTPListenerConfiguration(InetAddress.getByName("127.0.0.1"), 3000)).withHandler((req, resp) -> {
-       resp.setContentType("text/html; charset=utf-8");
-       resp.getOutputStream().write("Olá mundo".getBytes());
-       
+   HTTPServer serv = new HTTPServer().withListener(new HTTPListenerConfiguration(5000)).withHandler((req, resp) ->{
+       resp.getOutputStream().write("Hello world".getBytes());
    }).start();
    
   
